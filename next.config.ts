050da4714be7a041815ca7f19818e71bd7e62c6b
m0kyoji <1673517+m0kyoji@users.noreply.github.com>
2024-10-27 13:57:45 +0900

    fix: pwa

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,20 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-})
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
 
 module.exports = withPWA({
   // 他の Next.js の設定
